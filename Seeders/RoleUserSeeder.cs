1d2ffb710f6cde4b4253c51d8afab455433f2612
Mohammad Nasyikh Al-Qusyairy <nasyikhqusyairy@gmail.com>
2024-08-08 15:55:59 +0700

    chore: Refactor RoleUserSeeder to remove unnecessary code and improve efficiency

--- Seeders/RoleUserSeeder.cs
+++ Seeders/RoleUserSeeder.cs
@@ -11,23 +11,10 @@ namespace PEMIRA.Seeders
         {
             if (DBContext.RoleUsers.Any()) return;
 
-            List<Role> roles = [.. DBContext.Roles];
-            List<User> users = [.. DBContext.Users];
             List<RoleUser> items = [
-                new() {
-                    RoleId = roles[0].Id,
-                    UserId = users[0].Id
-                }
+
             ];
 
-            for (int i = 0; i < 10; i++)
-            {
-                items.Add(new RoleUser
-                {
-                    RoleId = roles[3].Id,
-                    UserId = users[i + 1].Id
-                });
-            }
             DBContext.RoleUsers.AddRange(items);
             DBContext.SaveChanges();
         }
