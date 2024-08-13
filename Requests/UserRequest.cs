2646be659ee59ab7d17648094826c0a15eff995c
Dzulfikar Al Ghozali <dzulfikaralghozali9@gmail.com>
2024-08-14 01:11:24 +0700

    Add User Done

---
+++ Requests/UserRequest.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using PEMIRA.Helpers;
+using PEMIRA.Interfaces;
+using PEMIRA.ViewModels;
+using PEMIRA.Models;
+using PEMIRA.Services;
+
+namespace PEMIRA.Requests;
+
+public class UserRequest(ModelStateDictionary modelState, UserViewModel input, UserService service) : IRequest<UserViewModel, UserService>
+{
+    public Dictionary<string, dynamic> DerivedData { get; set; } = [];
+    public ModelStateDictionary ModelState { get; set; } = modelState;
+    public UserViewModel UserInput { get; set; } = input;
+    public UserService Service { get; set; } = service;
+
+    public bool Validate()
+    {
+        if (Service.GetUserByCode(UserInput.Code) != null)
+        {
+            ModelState.AddModelError("Code", "Peserta Sudah Ada");
+        }
+        if (ModelState.IsValid)
+        {
+            DerivedData["User"] = ModelHelper.MapProperties<UserViewModel, User>(UserInput);
+        }
+        return ModelState.IsValid;
+    }
+
+}
---
+++ ViewModels/UserViewModel.cs
@@ -0,0 +1,31 @@
+using PEMIRA.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace PEMIRA.ViewModels
+{
+    public class UserViewModel
+    {
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "Nama harus diisi")]
+        [RegularExpression(@"^[a-zA-Z0-9\s\.\,\']+$", ErrorMessage = "Nama hanya boleh berisi huruf, angka, spasi, titik, koma, dan tanda petik")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "ID harus diisi")]
+        [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "ID hanya boleh berisi huruf dan angka")]
+        public string? Code { get; set; }
+
+        [Required(ErrorMessage = "Jenis Kelamin harus diisi")]
+        public bool? Gender { get; set; }
+
+        public string? More { get; set; }
+
+        [Required(ErrorMessage = "Password harus diisi")]
+        public string? Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Konfirmasi Password tidak sama dengan Password")]
+        public string? ConfirmPassword { get; set; }
+        public List<User> Users { get; set; } = [];
+
+    }
+}
