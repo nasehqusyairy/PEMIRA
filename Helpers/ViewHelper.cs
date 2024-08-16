f56d9dbcc5eaeb252c990a29f26a58b067bcd036
Mohammad Nasyikh Al-Qusyairy <nasyikhqusyairy@gmail.com>
2024-08-16 22:43:56 +0700

    Refactor ViewHelper to improve code readability and maintainability

--- Helpers/ViewHelper.cs
+++ Helpers/ViewHelper.cs
@@ -4,28 +4,28 @@ namespace PEMIRA.Helpers;
 
 public class ViewHelper
 {
-  public static IHtmlContent GenerateSearchAndLimitForm(int limitEntry, string searchQuery, string namepage)
-  {
-    var htmlContent = $@"
+    public static IHtmlContent GenerateSearchAndLimitForm(int limitEntry, string searchQuery, string placeholder = "Find...")
+    {
+        var htmlContent = $@"
             <div class='input-group mb-3'>
                 <input type='number' class='form-control' name='LimitEntry' value='{limitEntry}'
                     placeholder='Jumlah baris per halaman...(max 100)' max='100'>
                 <input type='text' class='form-control' name='Search' value='{searchQuery}' 
-                    placeholder='Cari {namepage}....'>
+                    placeholder='{placeholder}'>
                 <button class='btn btn-primary' type='submit'><i class='bi bi-search'></i></button>
             </div>";
 
-    return new HtmlString(htmlContent);
-  }
+        return new HtmlString(htmlContent);
+    }
 
-  public static IHtmlContent GeneratePagination(string currentUrl, int currentPage, int pageCount)
-  {
-    pageCount = pageCount < 1 ? 1 : pageCount;
-    currentPage = currentPage < 1 ? 1 : currentPage > pageCount ? pageCount : currentPage;
-    var previousPageUrl = UrlHelper.PreviousPage(currentUrl, currentPage);
-    var nextPageUrl = UrlHelper.NextPage(currentUrl, currentPage, pageCount);
+    public static IHtmlContent GeneratePagination(string currentUrl, int currentPage, int pageCount)
+    {
+        pageCount = pageCount < 1 ? 1 : pageCount;
+        currentPage = currentPage < 1 ? 1 : currentPage > pageCount ? pageCount : currentPage;
+        var previousPageUrl = UrlHelper.PreviousPage(currentUrl, currentPage);
+        var nextPageUrl = UrlHelper.NextPage(currentUrl, currentPage, pageCount);
 
-    var paginationHtml = $@"
+        var paginationHtml = $@"
             <div class='d-flex align-items-center justify-content-end'>
                 <span>Halaman</span>
                 <div class='mx-1'>
@@ -46,18 +46,18 @@ public class ViewHelper
                 <span>dari {pageCount}</span>
             </div>";
 
-    return new HtmlString(paginationHtml);
-  }
+        return new HtmlString(paginationHtml);
+    }
 
-  public static IHtmlContent GenerateNoDataRow(int colspan, string message = "Data tidak ditemukan")
-  {
-    var htmlContent = $@"
+    public static IHtmlContent GenerateNoDataRow(int colspan, string message = "Data tidak ditemukan")
+    {
+        var htmlContent = $@"
             <tr>
                 <td colspan='{colspan}' class='text-center'>{message}</td>
             </tr>";
 
-    return new HtmlString(htmlContent);
-  }
+        return new HtmlString(htmlContent);
+    }
 
 
 }
